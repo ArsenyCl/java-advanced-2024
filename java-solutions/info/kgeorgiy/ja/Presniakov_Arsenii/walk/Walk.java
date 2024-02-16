84e64f41373a7e796273f5c8f1a015dad63d1aae
Prseniakov Arsenii <presnyakov04@inbox.ru>
2024-02-16 16:07:49 +0300

    fixed

--- java-solutions/info/kgeorgiy/ja/Presniakov_Arsenii/walk/Walk.java
+++ java-solutions/info/kgeorgiy/ja/Presniakov_Arsenii/walk/Walk.java
@@ -2,19 +2,25 @@ package info.kgeorgiy.ja.Presniakov_Arsenii.walk;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-
+import java.nio.file.*;
 
 public class Walk {
     static final String invalidHash = "00000000";
     static final int bufferSize = 1024;
 
-    private static String readAndGetHash(File file)  {
+    private static String readAndGetHash(String file) {
 
         byte[] bytes = new byte[bufferSize];
         int hash = 0;
         int haveRead;
 
-        try (InputStream fileStream = new FileInputStream(file)) {
+        Path path;
+        try {
+            path = Path.of(file);
+        } catch (InvalidPathException e) {
+            return invalidHash;
+        }
+        try (InputStream fileStream = Files.newInputStream(path)) {
             while ((haveRead = fileStream.read(bytes)) >= 0) {
                 for (int i = 0; i < haveRead; i++) {
                     hash += bytes[i] & 0xff;
@@ -23,9 +29,6 @@ public class Walk {
                 }
             }
 
-        } catch (FileNotFoundException e) {
-            System.err.printf("FileNotFoundException in readAndHash: " + e.getLocalizedMessage());
-            return invalidHash;
         } catch (IOException e) {
             System.err.printf("IOException in readAndHash: " + e.getLocalizedMessage());
             return invalidHash;
@@ -44,35 +47,44 @@ public class Walk {
             return;
         }
 
-        File inputfile;
-        File outputfile;
+        Path inputfile;
+        Path outputfile;
 
 
         try {
-            inputfile = new File(args[0]);
-            outputfile = new File(args[1]);
-            outputfile.getParentFile().mkdirs();
+            inputfile = Path.of(args[0]);
+            outputfile = Path.of(args[1]);
+            if (outputfile.getParent() != null) {
+                Files.createDirectories(outputfile.getParent());
+            }
         } catch (NullPointerException e) {
             System.err.printf("NullPointerException in main: " + e.getLocalizedMessage());
             return;
         } catch (SecurityException e) {
             System.err.printf("SecurityException in main: " + e.getLocalizedMessage());
             return;
-        } catch (RuntimeException e) {
-            System.err.printf("RuntimeException in main: " + e.getLocalizedMessage());
+        } catch (InvalidPathException e) {
+            System.err.printf("InvalidPathException in main: " + e.getLocalizedMessage());
+            return;
+        } catch (IOException e) {
+            System.err.printf("IOException in main: " + e.getLocalizedMessage());
             return;
         }
 
         try (
-                BufferedReader reader = new BufferedReader(new FileReader(inputfile, StandardCharsets.UTF_8));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(outputfile, StandardCharsets.UTF_8))
+                BufferedReader reader = Files.newBufferedReader(inputfile, StandardCharsets.UTF_8);
         ) {
-            String stringFile;
-            while ((stringFile = reader.readLine()) != null) {
-                writer.write(readAndGetHash(new File(stringFile)) + " " + stringFile + System.lineSeparator());
+            try (BufferedWriter writer = Files.newBufferedWriter(outputfile, StandardCharsets.UTF_8)) {
+                String stringFile;
+                while ((stringFile = reader.readLine()) != null) {
+                    writer.write(readAndGetHash(stringFile) + " " + stringFile + System.lineSeparator());
+                }
+            } catch (IOException e) {
+                System.err.printf("IOException in main writer: " + e.getLocalizedMessage());
             }
+
         } catch (IOException e) {
-            System.err.printf("IOException in main: " + e.getLocalizedMessage());
+            System.err.printf("IOException in main reader: " + e.getLocalizedMessage());
         }
     }
 }
