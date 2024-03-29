7d2c28c3373bbd2a624ff7a1d1c65cf5ffc81a2a
Prseniakov Arsenii <presnyakov04@inbox.ru>
2024-03-29 16:23:05 +0300

    sorry for tons of files

---
+++ scripts/makejar.bat
@@ -0,0 +1,5 @@
+javac -d . -cp ..\..\java-advanced-2024\artifacts\info.kgeorgiy.java.advanced.implementor.jar ..\java-solutions\info\kgeorgiy\ja\Presniakov_Arsenii\implementor\Implementor.java
+
+jar cmvf .\META-INF\MANIFEST.MF Implementor.jar .\info\kgeorgiy\ja\Presniakov_Arsenii\implementor\*.class
+
+pause
---
+++ scripts/makejavadoc.bat
@@ -0,0 +1,1 @@
+javadoc -private -cp ..\..\java-advanced-2024\artifacts\info.kgeorgiy.java.advanced.implementor.jar -d ..\javadoc ..\java-solutions\info\kgeorgiy\ja\Presniakov_Arsenii\implementor\Implementor.java
